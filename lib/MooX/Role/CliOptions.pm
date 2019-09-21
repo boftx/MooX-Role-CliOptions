3861e39d1e5d0c61dfebef8fe3f23c4a7981bee4
Jim Bacon <boftx@hotmail.com>
2019-09-21 16:54:07 -0700

    Corrected version numbers.

--- lib/MooX/Role/CliOptions.pm
+++ lib/MooX/Role/CliOptions.pm
@@ -15,7 +15,7 @@ use Types::Standard qw( ArrayRef Bool );
 
 use Moo::Role;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 my @options = ( 'help', 'man' );
 do {
@@ -96,7 +96,7 @@ MooX::Role::CliOptions - Wrapper to simplify using Moo with Getopt::Long
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =head1 SYNOPSIS
 
